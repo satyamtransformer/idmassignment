713627d4442b1cd3e52ee3be73f19ca2cc8b0023
Satyam Gupta <s.p.gupta@student.tudelft.nl>
2024-02-29 04:23:50 +0100

    Task 1 is completed, can use as reference

--- src/main/java/tudelft/wis/idm_tasks/JDBCTask2.java
+++ src/main/java/tudelft/wis/idm_tasks/JDBCTask2.java
@@ -2,12 +2,8 @@ package tudelft.wis.idm_tasks;
 
 import tudelft.wis.idm_tasks.basicJDBC.interfaces.JDBCTask2Interface;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Properties;
+import java.sql.*;
+import java.util.*;
 
 public class JDBCTask2 implements JDBCTask2Interface {
     private Connection conn;
@@ -19,22 +15,27 @@ public class JDBCTask2 implements JDBCTask2Interface {
     public static void main(String[] args) {
 
         JDBCTask2 j = null;
+        // Task 1: List all primary titles for a specific start year.
         try {
             j = new JDBCTask2();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
             JDBCTask2 hdbc = new JDBCTask2();
+            Collection<String> pmTitle = j.getTitlesPerYear(2002);
+            Iterator<String> it = pmTitle.iterator();
+            while(it.hasNext()){
+                System.out.println(it.next());
+                it.remove();
+            }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
-        try {
-            j.getTitlesPerYear(2022);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        //Task 2: List all the job categories for titles that include a specific string in their primary title.
+
+        //Task 3 : List the average runtime of a specified genre.
+
+        //Task 4: Given a person’s full name, list all the characters they have played.
+
 
 
     }
@@ -45,7 +46,7 @@ public class JDBCTask2 implements JDBCTask2Interface {
             String url = "jdbc:postgresql://localhost:5432/imdb";
             Properties props = new Properties();
             props.setProperty("user", "postgres");
-            props.setProperty("password", "Satyam1234");
+            props.setProperty("password", "Satyam1234"); // change with you local password
             conn = DriverManager.getConnection(url, props);
         }
         return conn;
@@ -53,20 +54,37 @@ public class JDBCTask2 implements JDBCTask2Interface {
 
     @Override
     public Collection<String> getTitlesPerYear(int year) throws SQLException {
-        Connection conn = getConnection();
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+        Collection<String> pmTitle= new ArrayList<>();
 
-        conn.setAutoCommit(false);
-        PreparedStatement changeNameStmt = conn.prepareStatement(
-            "SELECT * FROM persons"
-        );
-//        changeNameStmt.setString(1, "Jean Grey-Summers");
-//        changeNameStmt.setString(2, "Jean Grey");
-//        changeNameStmt.executeUpdate();
-//        changeNameStmt.setString(1, "Scott Grey-Summers");
-//        changeNameStmt.setString (2, "Scott Summers");
-//        changeNameStmt.executeUpdate();
-        conn.commit();
-        return null;
+        try{
+            conn = getConnection();
+            // 1. Create SQL statement
+            String sql = "SELECT t.primary_title FROM titles t WHERE t.start_year = ?";
+
+            // 2. Create a preparred statement
+            ps = conn.prepareStatement(sql);
+
+            // 3. Insert parameter value(s) into prepared statement, the bases start from 0
+            ps.setInt(1, year);
+
+            // 4. Execute the prepared statement
+            resultSet = ps.executeQuery();
+
+            // 5. Process result
+
+            while (resultSet.next()){
+                pmTitle.add(resultSet.getString("primary_title"));
+            }
+
+
+        }catch(SQLException e){
+            throw new RuntimeException();
+        }
+
+        return pmTitle;
     }
 
     @Override
